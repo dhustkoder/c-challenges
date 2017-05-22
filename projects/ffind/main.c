9eedc3c081311db692ca232525a86b13adca8afc
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-05-22 15:32:01 +0000

    ffind dirent -> fts

--- projects/ffind/main.c
+++ projects/ffind/main.c
@@ -1,105 +1,62 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <pthread.h>
-#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <errno.h>
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fts.h>
+#include <pthread.h>
 
-static char* foundbuffer = NULL;
-static int foundbufferidx = 0;
 
-
-static inline void addfound(const char* const dirname, const char* const filename)
+static inline int compare(const FTSENT** a, const FTSENT** b)
 {
-	const int len = foundbufferidx + strlen(dirname) + strlen(filename) + 2;
-
-	foundbuffer = realloc(foundbuffer, len + 1);
-	sprintf(&foundbuffer[foundbufferidx], "%s/%s\n", dirname, filename);
-	foundbufferidx = len;
+	return strcmp((*a)->fts_name, (*b)->fts_name);
 }
 
 
-static inline bool stfind(const char* const dirname, const char* const filename)
+static inline int stfind(char* const rootdir, const char* const target)
 {
-	DIR* const dirp = opendir(dirname);
-	if (dirp == NULL) {
-		fprintf(stderr, "Couldn't open directory \"%s\": %s\n",
-		        dirname, strerror(errno));
-		return false;
+	char* path[] = { rootdir, NULL };
+
+	FTS* const ftsp = fts_open(path, 0, &compare);
+
+	if (ftsp == NULL) {
+		fprintf(stderr, "Couldn't open \"%s\": %s", rootdir, strerror(errno));
+		return EXIT_FAILURE;
 	}
 
-	const struct dirent* ent;
-	bool ret = false;
+	const FTSENT *parent, *child;
 
-	while ((ent = readdir(dirp)) != NULL) {
-		if (strcmp(filename, ent->d_name) == 0) {
-			addfound(dirname, filename);
-			ret = true;
-			break;
-		}
-	}
+	while ((parent = fts_read(ftsp)) != NULL) {
+		child = fts_children(ftsp, 0);
 
-	rewinddir(dirp);
-
-	int len = 0;
-	char* fullpath = NULL;
-	struct stat st;
-
-	while ((ent = readdir(dirp)) != NULL) {
-		if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
+		if (errno != 0) {
+			fprintf(stderr, "%s: %s\n", parent->fts_path, strerror(errno));
 			continue;
-
-		const int newlen = strlen(dirname) + strlen(ent->d_name);
-		if (newlen > len) {
-			free(fullpath);
-			fullpath = malloc(newlen + 2);
-			len = newlen;
 		}
 
-		sprintf(fullpath, "%s/%s", dirname, ent->d_name);
-		stat(fullpath, &st);
-		if (S_ISDIR(st.st_mode) && !S_ISLNK(st.st_mode) && access(fullpath, R_OK) == 0) {
-			const bool found = stfind(fullpath, filename);
-			if (!ret && found)
-				ret = true;
+		while (child != NULL) {
+			if (strcmp(child->fts_name, target) == 0)
+				printf("%s/%s\n", child->fts_path, child->fts_name);
+			child = child->fts_link;
 		}
 	}
 
-	free(fullpath);
-	closedir(dirp);
-	return ret;
-}
-
-
-static inline int mtfind(const char* const root, const char* const file)
-{
+	fts_close(ftsp);
 	return EXIT_SUCCESS;
 }
 
 
-int main(const int argc, const char* const* argv)
+int main(const int argc, char* const* argv)
 {
 	if (argc < 3) {
 		fprintf(stderr, "Usage: %s [directory] [file]\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
-	const char* const root = argv[1];
-	const char* const file = argv[2];
-	const int r1 = stfind(root, file);
-
-	if (r1 == EXIT_FAILURE) {
-		puts("NOT FOUND");
-		return EXIT_FAILURE;
-	}
-
-	printf("FOUND:\n%s", foundbuffer);
-	free(foundbuffer);
-	return EXIT_SUCCESS;
+	return stfind(argv[1], argv[2]);
 }
 
