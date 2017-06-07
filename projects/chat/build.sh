5acc2013d15653ab7d9222d078d6e01af1395fba
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-06-07 13:25:52 +0000

    removed link miniupnpc

--- projects/chat/build.sh
+++ projects/chat/build.sh
@@ -3,7 +3,7 @@ PROJDIR=$(dirname "$0")
 CC="$1"
 CFLAGS="$2"
 OUTDIR="$3"
-LIBS="-lminiupnpc"
+
 
 echo "${CC} ${CFLAGS} ${LIBS} ${PROJDIR}/main.c -o ${OUTDIR}"
 $CC $CFLAGS $LIBS $PROJDIR/main.c -o $OUTDIR
