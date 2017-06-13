c0fd19019a98df5c5fd662d0872b994813b23e05
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-06-13 19:53:37 +0000

    ncurses chat running

--- projects/chat/build.sh
+++ projects/chat/build.sh
@@ -6,8 +6,5 @@ OUTDIR="$3"
 LIBS="-lminiupnpc -lncurses"
 
 echo "${CC} ${CFLAGS} ${LIBS} ${PROJDIR}/main.c -o ${OUTDIR}"
-$CC $CFLAGS $LIBS $PROJDIR/main.c $PROJDIR/network.c $PROJDIR/upnp.c -o $OUTDIR
+$CC $CFLAGS $LIBS $PROJDIR/main.c $PROJDIR/chat.c $PROJDIR/network.c $PROJDIR/upnp.c -o $OUTDIR
 
-
-echo "$CC $CFLAGS $LIBS $PROJDIR/ncurses-test.c -o $(dirname $OUTDIR)/ncurses"
-$CC $CFLAGS $LIBS $PROJDIR/ncurses_ui.c -o $(dirname $OUTDIR)/ncurses
---
+++ projects/chat/chat.c
@@ -0,0 +1,264 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <locale.h>
+#include <ncurses.h>
+#include "utils/io.h"
+#include "network.h"
+
+
+#define CHAT_STACK_SIZE ((int)24)
+#define BUFFER_SIZE     ((int)512)
+
+static const ConnectionInfo* connection_info = NULL;     // connection information
+static char conn_buffer[BUFFER_SIZE]         = { '\0' }; // buffer for incoming msgs
+static char* chatstack[CHAT_STACK_SIZE]      = { NULL }; // the chat msg stack with unames
+static int chatstack_idx                     = 0;        // current chat stack index
+
+char buffer[BUFFER_SIZE]; // text box's buffer for user input
+int blen;                 // text box's current buffer input size
+int bidx;                 // text box's cursor position in the buffer
+int cy, cx;               // current cursor position in the window
+int my, mx;               // max y and x positions
+int hy, hx;               // text box's home y and x (start position)
+
+static void printChat(void);
+static inline void initialize_ncurses(void)
+{
+	initscr();
+	noecho();
+	timeout(0);
+	keypad(stdscr, TRUE);
+	clear();
+	move(0, 0);
+	printChat();
+	getmaxyx(stdscr, my, mx);
+	getyx(stdscr, hy, hx);
+	cy = hy;
+	cx = hx;
+}
+
+
+static inline void terminate_ncurses(void)
+{
+	endwin();
+}
+
+
+static inline void clearTextBox(void)
+{
+	cy = hy;
+	cx = hx;
+	blen = 0;
+	bidx = 0;
+	buffer[0] = '\0';
+}
+
+
+static inline void moveCursorLeft(void)
+{
+	if (cy > hy || cx > hx) {
+		--bidx;
+		--cx;
+		if (cx < 0) {
+			cx = mx - 1;
+			--cy;
+		}
+		move(cy, cx);
+	}
+}
+
+
+static inline void moveCursorRight(void)
+{
+	if (bidx < blen) {
+		++bidx;
+		++cx;
+		if (cx >= mx) {
+			cx = 0;
+			++cy;
+		}
+		move(cy, cx);
+	}
+}
+
+
+static inline void moveCursorHome(void)
+{
+	if (bidx != 0) {
+		bidx = 0;
+		cy = hy;
+		cx = hx;
+		move(cy, cx);
+	}
+}
+
+
+static inline void moveCursorEnd(void)
+{
+	if (bidx < blen) {
+		bidx = blen;
+		cx = hx + (blen % mx);
+		cy = hy + (blen / mx);
+		move(cy, cx);
+	}
+}
+
+
+static inline void printChat(void)
+{
+	printw("Host: %s (%s). Client: %s (%s).\n"
+	       "=================================================\n",
+	       connection_info->host_uname, connection_info->host_ip,
+	       connection_info->client_uname, connection_info->client_ip);
+
+	int i;
+	for (i = 0; i < chatstack_idx; ++i)
+		printw("%s\n", chatstack[i]);
+	for (; i < CHAT_STACK_SIZE; ++i)
+		printw("\n");
+
+	printw("==================================================\n> ");
+}
+
+
+static inline void refreshUI(void)
+{
+	clear();
+	move(0, 0);
+	printChat();
+	printw(buffer);
+	move(cy, cx);
+	refresh();
+}
+
+
+static inline void handleWindowResize(void)
+{
+	clear();
+	move(0, 0);
+	printChat();
+	getyx(stdscr, hy, hx);
+	getmaxyx(stdscr, my, mx);
+	cy = hy + (bidx / mx);
+	cx = hx + (bidx % mx);
+}
+
+
+static inline bool updateTextBox(void)
+{
+	const int c = getch();
+
+	switch (c) {
+	case 10: // also enter (ascii) [fall]
+	case KEY_ENTER: // submit msg
+		return true;
+	case KEY_LEFT:
+		moveCursorLeft();
+		return false;
+	case KEY_RIGHT:
+		moveCursorRight();
+		return false;
+	case 127: // also backspace (ascii) [fall]
+	case KEY_BACKSPACE:
+		if (bidx > 0) {
+			memmove(&buffer[bidx - 1],
+			        &buffer[bidx],
+				blen - bidx);
+			buffer[--blen] = '\0';
+			moveCursorLeft();
+			refreshUI();
+		}
+		return false;
+	case KEY_HOME:
+		moveCursorHome();
+		return false;
+	case KEY_END:
+		moveCursorEnd();
+		return false;
+	case KEY_RESIZE:
+		handleWindowResize();
+		refreshUI();
+		return false;
+	}
+
+	if (isascii(c) && blen < BUFFER_SIZE) {
+		if (bidx < blen) {
+			memmove(&buffer[bidx + 1],
+			        &buffer[bidx],
+				blen - bidx);
+		}
+		buffer[bidx] = (char) c;
+		buffer[++blen] = '\0';
+		moveCursorRight();
+		refreshUI();
+	}
+
+	return false;
+}
+
+
+
+static inline bool stackMsg(const char* const uname, const char* const msg)
+{
+	if (chatstack_idx >= CHAT_STACK_SIZE) {
+		free(chatstack[0]);
+		for (int i = 0; i < chatstack_idx - 1; ++i)
+			chatstack[i] = chatstack[i + 1];
+		chatstack_idx = CHAT_STACK_SIZE - 1;
+	}
+
+	const int len = strlen(msg) + strlen(uname) + 3;
+	chatstack[chatstack_idx] = malloc(len + 1);
+	sprintf(chatstack[chatstack_idx], "%s: %s", uname, msg);
+	++chatstack_idx;
+	return true;
+}
+
+
+static inline void freeMsgStack(void)
+{
+	for (int i = 0; i < chatstack_idx; ++i)
+		free(chatstack[i]);
+}
+
+
+
+static inline bool checkfd(const int fd)
+{
+	struct timeval timeout = { 0, 0 };
+	fd_set fds;
+
+	FD_ZERO(&fds);
+	FD_SET(fd, &fds);
+
+	return select(fd + 1, &fds, NULL, NULL, &timeout) > 0;
+}
+
+
+int chat(const ConnectionInfo* const cinfo)
+{
+	connection_info = cinfo;
+	initialize_ncurses();
+	refreshUI();
+
+	for (;;) {
+
+		if (checkfd(cinfo->remote_fd)) {
+			readInto(conn_buffer, cinfo->remote_fd, BUFFER_SIZE);
+			stackMsg(cinfo->remote_uname, conn_buffer);
+			refreshUI();
+		} else if (updateTextBox()) {
+			writeInto(cinfo->remote_fd, buffer);
+			stackMsg(cinfo->local_uname, buffer);
+			clearTextBox();
+			refreshUI();
+		}
+	}
+
+	terminate_ncurses();
+	freeMsgStack();
+	return EXIT_SUCCESS;
+}
+
